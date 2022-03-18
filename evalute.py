9f040cd3765012b7c6488391a710e40a63bbc08e
Maple-Lazuli <the.nostra.tymus@gmail.com>
2022-03-18 13:10:31 -0400

    added INEMB block to the evaluate,py script for convience

--- evalute.py
+++ evalute.py
@@ -2,33 +2,35 @@ import tensorflow as tf
 from dataset_generator import DatasetGenerator
 from lenet_mnist_graph import parse_records
 
-location = "./mnist_tf/test/mnist_test.tfrecords"
-test_df = DatasetGenerator(location, parse_function=parse_records, shuffle=True,
-                               batch_size=1)
-iterator = test_df.get_iterator()
-next_step = iterator.get_next()
+if __name__ == '__main__':
 
-# experiment with mnist since the model is small and stored in github
-sess = tf.compat.v1.Session()
-# TODO Figure out what the error means
-saver = tf.compat.v1.train.import_meta_graph("./mnist_model/mnist.meta")
-saver.restore(sess, tf.compat.v1.train.latest_checkpoint("./mnist_model/"))
+    location = "./mnist_tf/test/mnist_test.tfrecords"
+    test_df = DatasetGenerator(location, parse_function=parse_records, shuffle=True,
+                                   batch_size=1)
+    iterator = test_df.get_iterator()
+    next_step = iterator.get_next()
 
-graph = tf.compat.v1.get_default_graph()
+    # experiment with mnist since the model is small and stored in github
+    sess = tf.compat.v1.Session()
+    # TODO Figure out what the error means
+    saver = tf.compat.v1.train.import_meta_graph("./mnist_model/mnist.meta")
+    saver.restore(sess, tf.compat.v1.train.latest_checkpoint("./mnist_model/"))
 
-input_image = graph.get_tensor_by_name("mnist_model/X:0")
-predicted_label = graph.get_tensor_by_name("mnist_model/y_pred:0")
+    graph = tf.compat.v1.get_default_graph()
 
-try:
-    sess.run(iterator.initializer)
-    while True:
-        features = sess.run(next_step)
-        batch_x = features['input']
-        batch_y = features['label']
-        # test sucessful data export
-        print(batch_y)
-except tf.errors.OutOfRangeError:
-    print("done")
+    input_image = graph.get_tensor_by_name("mnist_model/X:0")
+    predicted_label = graph.get_tensor_by_name("mnist_model/y_pred:0")
+
+    try:
+        sess.run(iterator.initializer)
+        while True:
+            features = sess.run(next_step)
+            batch_x = features['input']
+            batch_y = features['label']
+            # test sucessful data export
+            print(batch_y)
+    except tf.errors.OutOfRangeError:
+        print("done")
 
 """
 ValueError: Converting GraphDef to Graph has failed with an error: 'Cannot add function 
